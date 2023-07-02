26d635bf10eec53529aea4cada6f23cb26482e15
SagertStav <biryukovsrg@yandex.ru>
2023-07-02 04:00:45 +0300

    ДЗ4 - реализовал в video.py тройное наследование Channel>Video>PLVideo. Добился ожидаемого поведения в homework-4\main

--- src/channel.py
+++ src/channel.py
@@ -24,9 +24,33 @@ class Channel:
         self.title= channel["items"][0] ['snippet']['title']                         # - название канала
         self.description= channel['items'][0]["snippet"]["description"]              # - описание канала
         self.url = 'https://www.youtube.com/channel/'+channel_id                     # - ссылка на канал
-        self.subscriber_count = channel['items'][0]["statistics"]["subscriberCount"] # - количество подписчиков
+        self.subscriber_count = int(channel['items'][0]["statistics"]["subscriberCount"]) # - количество подписчиков
         self.video_count = channel['items'][0]["statistics"]["videoCount"]           # - количество видео
         self.view_count = channel['items'][0]["statistics"]["viewCount"]             # - общее количество просмотров
+
+    def __str__(self):
+        return f"'{self.title} ({self.url})'"
+
+    def __add__(self, other):
+        return self.subscriber_count + other.subscriber_count
+
+    def __sub__(self, other):
+        return self.subscriber_count - other.subscriber_count
+
+    def __gt__(self, other):
+        return self.subscriber_count > other.subscriber_count
+
+    def __ge__(self, other):
+        return self.subscriber_count >= other.subscriber_count
+
+    def __lt__(self, other):
+        return self.subscriber_count < other.subscriber_count
+
+    def __le__(self, other):
+        return self.subscriber_count <= other.subscriber_count
+
+    def __eq__(self, other):
+        return self.subscriber_count == other.subscriber_count
 
     # Геттер для channel_id, без создания сеттера
     @property
---
+++ src/video.py
@@ -0,0 +1,48 @@
+import src.channel
+class Video(src.channel.Channel):
+    """Класс для ВИДЕО ютуб - канала"""
+    def __init__(self, video_id) -> None:
+       """
+       Инициализация реальными данными атрибутов экземпляра класса `Video`:
+       - id видео
+       - название видео video_title
+       - ссылка на видео
+       - количество просмотров
+       - количество лайков
+       """
+       '''
+       получить статистику видео по его id
+       получить id можно из адреса видео
+       https://www.youtube.com/watch?v=gaoc9MPZ4bw или https://youtu.be/gaoc9MPZ4bw
+       '''
+       #video_id = 'gaoc9MPZ4bw'
+       video_response = self.get_service().videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                              id=video_id
+                                              ).execute()
+       #print(video_response)
+       self.__video_id = video_id                                                    # id видео
+       self.title:str = video_response['items'][0]['snippet']['title']         # название видео
+       self.url = 'https://www.youtu.be.com/'+video_id                               # ссылка на видео
+       self.view_count: int = video_response['items'][0]['statistics']['viewCount']  # количество просмотров
+       self.like_count: int = video_response['items'][0]['statistics']['likeCount']  # количество лайков
+
+    def __str__(self):
+        return f"{self.title}"
+
+class PLVideo(Video):
+    def __init__(self, video_id: str, playlist_id: str) -> None:
+        """ Инициализация по 'id видео' и 'id плейлиста'
+        > Видео может находиться в множестве плейлистов, поэтому непосредственно из видео через API информацию о плейлисте не получить.
+         Инициализация реальными данными атрибутов экземпляра подкласса PLVideo:
+        - id видео
+        - название видео
+        - ссылка на видео
+        - количество просмотров
+        - количество лайков
+        - id плейлиста  """
+
+        self.__playlist_id = self.playlist_id = playlist_id
+        super().__init__(video_id)
+
+
+
